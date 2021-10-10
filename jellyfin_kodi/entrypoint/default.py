79d1308a219a4669d24c5174ddf7044de5bea84e
Odd Stråbø <oddstr13@openshell.no>
2021-10-10 15:05:22 +0200

    Apply suggestions from code review

--- jellyfin_kodi/entrypoint/default.py
+++ jellyfin_kodi/entrypoint/default.py
@@ -242,7 +242,7 @@ def browse(media, view_id=None, folder=None, server_id=None):
 
         monitor = xbmc.Monitor()
 
-        for _ in range(300):
+        for _i in range(300):
             if window('jellyfin_online.bool'):
                 break
             elif monitor.waitForAbort(0.1):
